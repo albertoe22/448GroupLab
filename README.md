050c40aa52a87cab613afb9e97e020b8b5fd7901
Albert Truong <albertoe4788@gmail.com>
2021-02-11 21:12:35 +0000

    Read me, General project ideas or done

---
+++ README.md
@@ -0,0 +1,3 @@
+To view the project you can visit the page here: https://albertoe22.github.io/Lab1/
+
+Or you can visit the github page: https://github.com/albertoe22/Lab1
---
+++ script.js
@@ -0,0 +1,46 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#imageResult')
+                .attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(function () {
+    $('#upload').on('change', function () {
+        readURL(input);
+    });
+});
+function onClickSubmit(){
+  var submit = document.getElementById("submitbutton");
+  var load = document.getElementById("loading");
+  submit.style.display = "none";
+  load.style.display = "block";
+  setTimeout("submitappear()",3000);
+}
+function submitappear(){
+  var submit = document.getElementById("submitbutton");
+  var load = document.getElementById("loading");
+  submit.style.display = "block";
+  load.style.display = "none"; 
+  alert("Submit Successful");
+}
+var i = 0;
+
+function makeProgress() {
+  if (i < 100) {
+    i = i+1;
+  }
+  else {
+    i=0
+    readURL(document.getElementById("upload"));
+    return
+  }
+  $(".progress-bar").css("width", i + "%").text(i+" %");
+  
+  setTimeout("makeProgress()", 50);
+  
+}
